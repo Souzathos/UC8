e55bccc04ed0100de4e85c0c5ef2f0957f77889b
Souzathos <athosfelipe01@gmail.com>
2025-10-24 21:51:00 -0300

    Exercicio pokemon

---
+++ exercicioPokemon/app/src/main/java/exercicioPokemon/Pokemon.java
@@ -0,0 +1,24 @@
+package exercicioPokemon;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ATHOSFELIPENASCIMENT
+ */
+ public  class  Pokemon {
+    String nome;
+    String tipo;
+    int nivel;
+
+    public Pokemon(String nome, String tipo, int nivel) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+    
+    export Pokemon;
+}
